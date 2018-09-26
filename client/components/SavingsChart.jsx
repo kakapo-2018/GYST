6b6d3a623df0293d32cc6b1d0d4b8d972adb7908
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-27 10:34:40 +1200

    changing to liquid gauge

--- client/components/SavingsChart.jsx
+++ client/components/SavingsChart.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import ChartistGraph from "./Chartist";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class Bar extends Component {
   constructor(props) {
@@ -12,44 +17,52 @@ class Bar extends Component {
   }
 
   handleChange(e) {
-    console.log(e.target.value);
-    // this.setState({
-    //   savedMoney: e.target.value
-    // });
     this.setState({ savedMoney: e.target.value });
   }
 
   render() {
     var data = {
-      labels: ["Money Saved"],
+      labels: ["Saved " + this.state.savedMoney + " / " + this.state.totalGoal],
       series: [this.state.savedMoney]
     };
 
     var options = {
       donut: true,
-      donutWidth: 60,
+      donutWidth: 40,
       startAngle: 270,
       total: this.state.totalGoal,
       showLabel: true
+      //stroke: "#81C784"
     };
 
     var type = "Pie";
-    // var aspectRatio = "ct-octave";
 
     return (
-      <div>
-        <ChartistGraph
-          id="pie2"
-          className="ct-chart ct-major-tenth"
-          data={data}
-          options={options}
-          type={type}
-        />
-        <input type="number" onChange={this.handleChange} />
-      </div>
+      <Card style={{ padding: 0, maxWidth: 475 }}>
+        <CardContent style={{ padding: 0, maxWidth: 475 }}>
+          <div>
+            <ChartistGraph
+              id="pie2"
+              className="ct-chart ct-major-tenth"
+              data={data}
+              options={options}
+              type={type}
+              style={{ stroke: "#81C784" }}
+            />
+          </div>
+        </CardContent>
+        <CardActions>
+          <Typography component="p">Amt Saved:</Typography>
+          <input
+            placeholder="Amt Saved"
+            type="number"
+            onChange={this.handleChange}
+            style={{ padding: 0, maxWidth: "15%" }}
+          />
+        </CardActions>
+      </Card>
     );
   }
 }
 
 export default Bar;
-//ReactDOM.render(<Bar />, document.getElementById("react-chart"));
