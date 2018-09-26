845b0b65857568be6bc3ffcff9d3e268066710e3
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-27 11:20:33 +1200

    put into a card

--- client/components/Gauge.jsx
+++ client/components/Gauge.jsx
@@ -2,6 +2,11 @@ import { color } from "d3-color";
 import { interpolateRgb } from "d3-interpolate";
 import React, { Component } from "react";
 import LiquidFillGauge from "react-liquid-gauge";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class Gauge extends Component {
   state = {
@@ -12,7 +17,7 @@ class Gauge extends Component {
   endColor = "#4CAF50";
 
   render() {
-    const radius = 200;
+    const radius = 120;
     const interpolate = interpolateRgb(this.startColor, this.endColor);
     const fillColor = interpolate(this.state.value / this.state.savingGoal);
     const gradientStops = [
@@ -41,66 +46,71 @@ class Gauge extends Component {
     ];
 
     return (
-      <div>
-        <LiquidFillGauge
-          style={{ margin: "0 auto" }}
-          width={radius * 2}
-          height={radius * 2}
-          value={(this.state.value / this.state.savingGoal) * 100}
-          percent="%"
-          textSize={1}
-          textOffsetX={0}
-          textOffsetY={0}
-          textRenderer={props => {
-            const value = Math.round(props.value);
-            const radius = Math.min(props.height / 2, props.width / 2);
-            const textPixels = (props.textSize * radius) / 2;
-            const valueStyle = {
-              fontSize: textPixels
-            };
-            const percentStyle = {
-              fontSize: textPixels * 0.6
-            };
+      <Card style={{ maxWidth: 275 }}>
+        <CardContent style={{ padding: 0, maxWidth: 275 }}>
+          <LiquidFillGauge
+            style={{ margin: "0 auto" }}
+            width={radius * 2}
+            maxWidth={375}
+            height={radius * 2}
+            value={(this.state.value / this.state.savingGoal) * 100}
+            percent="%"
+            textSize={1}
+            textOffsetX={0}
+            textOffsetY={0}
+            textRenderer={props => {
+              const value = Math.round(props.value);
+              const radius = Math.min(props.height / 2, props.width / 2);
+              const textPixels = (props.textSize * radius) / 2;
+              const valueStyle = {
+                fontSize: textPixels
+              };
+              const percentStyle = {
+                fontSize: textPixels * 0.6
+              };
 
-            return (
-              <tspan>
-                <tspan className="value" style={valueStyle}>
-                  {value}
+              return (
+                <tspan>
+                  <tspan className="value" style={valueStyle}>
+                    {value}
+                  </tspan>
+                  <tspan style={percentStyle}>{props.percent}</tspan>
                 </tspan>
-                <tspan style={percentStyle}>{props.percent}</tspan>
-              </tspan>
-            );
-          }}
-          riseAnimation
-          waveAnimation
-          waveFrequency={2}
-          waveAmplitude={1}
-          gradient
-          gradientStops={gradientStops}
-          circleStyle={{
-            fill: fillColor
-          }}
-          waveStyle={{
-            fill: fillColor
-          }}
-          textStyle={{
-            fill: color("#444").toString(),
-            fontFamily: "Arial"
-          }}
-          waveTextStyle={{
-            fill: color("#fff").toString(),
-            fontFamily: "Arial"
-          }}
-        />
-        <div
-          style={{
-            margin: "20px auto",
-            width: 120
-          }}
-        >
+              );
+            }}
+            riseAnimation
+            waveAnimation
+            waveFrequency={2}
+            waveAmplitude={1}
+            gradient
+            gradientStops={gradientStops}
+            circleStyle={{
+              fill: fillColor
+            }}
+            waveStyle={{
+              fill: fillColor
+            }}
+            textStyle={{
+              fill: color("#444").toString(),
+              fontFamily: "Arial"
+            }}
+            waveTextStyle={{
+              fill: color("#fff").toString(),
+              fontFamily: "Arial"
+            }}
+          />
+        </CardContent>
+        <CardActions style={{ margin: "2%", display: "flex" }}>
+          {/* <div
+            style={{
+              margin: "20px auto",
+              width: 120
+            }}
+          > */}
           <input
+            style={{ maxWidth: "45%" }}
             type="number"
-            placeholder="savedAmount"
+            placeholder="Amount Saved"
             onChange={e => {
               this.setState({
                 value: e.target.value,
@@ -109,6 +119,7 @@ class Gauge extends Component {
             }}
           />
           <input
+            style={{ maxWidth: "45%" }}
             type="number"
             placeholder="Goal"
             onChange={e => {
@@ -118,8 +129,9 @@ class Gauge extends Component {
               });
             }}
           />
-        </div>
-      </div>
+          {/* </div> */}
+        </CardActions>
+      </Card>
     );
   }
 }
