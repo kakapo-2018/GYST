e92dad725fe26f481929b5122cb6774ad1b06ab4
lukechadwick <llukechadwick@gmail.com>
2018-09-27 15:49:40 +1200

    Fitness counter working with food search and calorie counter

--- client/components/Fitness.jsx
+++ client/components/Fitness.jsx
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit
+  }
+});
 
 class Fitness extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchInfo: '',
-      searchTerm: 'chicken burger'
+      searchTerm: '',
+      currentCals: 0,
+      totalCals: 0
     };
   }
 
-  componentDidMount() {
+  componentDidMount() {}
+
+  getData() {
     var foodWeb = require('foodweb');
 
     var item = foodWeb.search(this.state.searchTerm)[0];
@@ -31,17 +47,51 @@ class Fitness extends Component {
         this.state.searchTerm +
         ' is ' +
         serving +
-        ' calories'
+        ' calories',
+      totalCals: this.state.totalCals + serving
+    });
+    this.forceUpdate();
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      searchTerm: evt.target.value
     });
   }
 
+  submit = e => {
+    this.getData(this.state.searchTerm);
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
       <Card style={{ maxWidth: 275 }}>
         <CardContent style={{ padding: 0, maxWidth: 275 }}>
           <div>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={evt => this.updateInputValue(evt)}
+              className="form-control"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+            />
+            <Button
+              variant="fab"
+              color="primary"
+              aria-label="Add"
+              className={classes.button}
+              onClick={this.submit}
+            >
+              <AddIcon />
+            </Button>
+
             <h1>Food Trackr</h1>
             {this.state.searchInfo}
+            <h1>Total Cals</h1>
+            {this.state.totalCals}
           </div>
         </CardContent>
       </Card>
@@ -49,4 +99,4 @@ class Fitness extends Component {
   }
 }
 
-export default Fitness;
+export default withStyles(styles)(Fitness);
