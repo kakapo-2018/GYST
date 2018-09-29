939d0ba66d9e593c18265a2fc9553f818ab0ccb8
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-29 17:23:12 +1200

    text input

--- client/components/SocialFeed.jsx
+++ client/components/SocialFeed.jsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import InstagramEmbed from 'react-instagram-embed';
+import TextField from '@material-ui/core/TextField';
 
 const styles = {
   card: {
@@ -35,8 +36,15 @@ function SimpleCard(props) {
   return (
     <Card className={classes.card}>
       <CardContent>
+        <TextField
+          id="standard-textarea"
+          label="Add instagram image url"
+          placeholder="URL link"
+          multiline
+          margin="normal"
+        />
         <InstagramEmbed
-          url="https://instagr.am/p/Zw9o4/"
+          url="https://www.instagram.com/p/Zw9o4/"
           hideCaption={false}
           containerTagName="div"
           injectScript
