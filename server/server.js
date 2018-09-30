17810df9c3dddc633d83fa04af50a0aa50447436
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-30 19:47:38 +1300

    process env config

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = '5da992c3166447788d05de7a2d0c1b0c'; // Your client id
-var client_secret = 'c71b46dbe44c4515bfd4aaf26b8c6ace'; // Your secret
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
 if (port == 3000) {
   var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
 } else {
