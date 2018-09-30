8765004f7c2b6f065723eef735e80b3d17979526
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-01 09:56:52 +1300

    bells entered

--- client/components/AlarmList.jsx
+++ client/components/AlarmList.jsx
@@ -56,6 +56,14 @@ class AlarmList extends React.Component {
                 <AlarmEntry time={alarm.time} comment={alarm.comment} onClose={this.handleEntryClose.bind(this, i)} key={i} onRing={this.props.onRing} />
             );
         }.bind(this));
+
+        var bells = [
+            {name: 'piano-melody', type: 'audio/wav',  path: 'bell/70214__qlc__65bpm-piano-melody-0589.wav'},
+            {name: 'fractal-ramp-sonnet', type: 'audio/mpeg', path: 'bell/70002__qlc__240bpm-fractal-ramp-sonnet-track-1.mp3'},
+            {name: 'osng', type: 'audio/wav',  path: 'bell/70213__qlc__152bpm-osng.wav'},
+            {name: 'zichus', type: 'audio/wav',  path: 'bell/70217__qlc__85bpm-zichus.wav'}
+        ]
+        
         var list = function(){
             if(this.state.data.length == 0) {
                 return (<li className="list-group-item">None</li>);
