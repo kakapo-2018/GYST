10be8d23cdd52ae94df5023f8f95689100a9bbb0
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-01 10:01:54 +1300

    working on bell

--- client/components/Bell.jsx
+++ client/components/Bell.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = theme => ({
+    digit: {
+        width:'80px',
+        flexDirection: 'row',
+        display: 'inline-block',
+    }
+
+});
 
 
 class Bell extends React.Component {
+ 
+
     render() {
+        const { classes } = this.props;
+      
         return (
-            Bell
+        <div>Bell</div>
         )
     }
 }
 
+export default withStyles(styles)(Bell);
 
-export default Bell
