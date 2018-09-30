07a856be185df018f45993f416bf1d26cbfc5d3d
lukechadwick <llukechadwick@gmail.com>
2018-09-30 13:47:21 +1300

    Chat working

--- server/index.js
+++ server/index.js
@@ -3,6 +3,27 @@ const server = require('./server');
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, function() {
+app = server.listen(port, function() {
   console.log('Listening on port:', port);
+});
+
+var socket = require('socket.io');
+io = socket(app);
+
+// io.on('connection', socket => {
+//   console.log(socket.id);
+// });
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
