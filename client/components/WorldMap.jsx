abdb9aa6a1f5f632cfdda12d0ad72729fbdea586
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-01 20:01:48 +1300

    done

--- client/components/WorldMap.jsx
+++ client/components/WorldMap.jsx
@@ -7,7 +7,7 @@ import {
 } from 'react-simple-maps';
 import Card from '@material-ui/core/Card';
 import { withStyles } from '@material-ui/core/styles';
-
+import { get, set } from '../utils/localStorage';
 const wrapperStyles = {
   maxWidth: '100%',
   maxHeight: '100%',
@@ -30,11 +30,13 @@ const styles = theme => ({
 
 class WorldMap extends Component {
   state = {
-    place: ''
+    place: '',
+    clickArr: []
   };
 
   handleClick(i) {
     this.setState({
+      clickArr: this.state.clickArr.concat(i),
       place:
         this.state.place == ''
           ? this.state.place + i.properties.name
@@ -42,8 +44,12 @@ class WorldMap extends Component {
             ? this.state.place.replace(', ' + i.properties.name, '')
             : this.state.place + ', ' + i.properties.name
     });
+    setTimeout(() => {
+      set('state', JSON.stringify(this.state.place));
+    }, 200);
   }
   render() {
+    const persistent = get('state');
     const { classes } = this.props;
     return (
       <Card className={classes.card}>
@@ -73,20 +79,37 @@ class WorldMap extends Component {
                           }}
                           geography={geography}
                           projection={projection}
-                          style={{
-                            default: {
-                              fill: '#ECEFF1',
-                              stroke: '#607D8B',
-                              strokeWidth: 0.75,
-                              outline: 'none'
-                            },
-                            pressed: {
-                              fill: '#607D8B',
-                              stroke: 'grey',
-                              strokeWidth: 0.75,
-                              outline: 'none'
-                            }
-                          }}
+                          style={
+                            !persistent.includes(geography.properties.name)
+                              ? {
+                                  default: {
+                                    fill: '#ECEFF1',
+                                    stroke: '#607D8B',
+                                    strokeWidth: 0.75,
+                                    outline: 'none'
+                                  },
+                                  pressed: {
+                                    fill: '#607D8B',
+                                    stroke: 'grey',
+                                    strokeWidth: 0.75,
+                                    outline: 'none'
+                                  }
+                                }
+                              : {
+                                  default: {
+                                    fill: '#607D8B',
+                                    stroke: '#607D8B',
+                                    strokeWidth: 0.75,
+                                    outline: 'none'
+                                  },
+                                  pressed: {
+                                    fill: '#607D8B',
+                                    stroke: 'grey',
+                                    strokeWidth: 0.75,
+                                    outline: 'none'
+                                  }
+                                }
+                          }
                         />
                       )
                   )
@@ -94,7 +117,7 @@ class WorldMap extends Component {
               </Geographies>
             </ZoomableGroup>
           </ComposableMap>
-          <p className={classes.name}>{this.state.place}</p>
+          <p className={classes.name}>{persistent}</p>
         </div>
       </Card>
     );
