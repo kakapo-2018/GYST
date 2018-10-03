eb195ba0a9c0f067744008ad83212642feeb30d5
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-03 16:48:28 +1300

    test done

---
+++ tests/client/actions/todo.test.js
@@ -0,0 +1,77 @@
+import * as actions from '../../../client/actions/todo';
+import nock from 'nock';
+
+test('add todo will dispatch an action on success', done => {
+  const fakeTodo = [{ id: 2, todo: 'test' }];
+  const scope = nock('http://localhost:80')
+    .post('/api/v1/todo/save')
+    .reply(200, fakeTodo);
+  const secondExpected = {
+    type: 'GET_TODOS',
+    isFetching: false,
+    todos: fakeTodo,
+    notification: 1
+  };
+
+  const dispatch = jest.fn().mockImplementationOnce(action => {
+    expect(action).toEqual(secondExpected);
+    scope.done();
+    done();
+  });
+
+  actions.addTodosAction()(dispatch);
+});
+
+// test('receive weight action creator', () => {
+//   const fakeWeight = '20, 30';
+
+//   const expected = {
+//     type: 'GET_WEIGHT',
+//     isFetching: false,
+//     weight: fakeWeight
+//   };
+
+//   expect(actions.recieveWeight(fakeWeight)).toEqual(expected);
+// });
+
+// test('set weight action creator', () => {
+//   const fakeWeight = '20, 30';
+
+//   const expected = {
+//     type: 'SET_WEIGHT',
+//     isFetching: false,
+//     weight: fakeWeight
+//   };
+
+//   expect(actions.setWeight(fakeWeight)).toEqual(expected);
+// });
+
+// test('loading weight action creator', () => {
+//   const expected = {
+//     type: 'LOADING_WEIGHT',
+//     isFetching: true
+//   };
+
+//   expect(actions.gettingWeights()).toEqual(expected);
+// });
+
+// test('save weight will dispatch an action on success', () => {
+//   const fakeWeight = '20, 30';
+
+//   const scope = nock('http://localhost:80')
+//     .get('/weight/save')
+//     .reply(200, fakeWeight);
+
+//   const expected = {
+//     type: 'SET_WEIGHT',
+//     isFetching: false,
+//     weight: fakeWeight
+//   };
+
+//   const dispatch = jest.fn().mockImplementationOnce(action => {
+//     expect(action).toEqual(expected);
+//     scope.done();
+//   });
+
+//   actions.saveWeightAction(dispatch);
+// });
