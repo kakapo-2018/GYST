28effe805553fa8b45990aadc8938f3c3f14a450
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-03 17:40:43 +1300

    testing

--- tests/client/actions/todo.test.js
+++ tests/client/actions/todo.test.js
@@ -22,6 +22,25 @@ test('add todo will dispatch an action on success', done => {
   actions.addTodosAction()(dispatch);
 });
 
+test('get todo will dispatch an action on success', done => {
+  const fakeTodo = [{ id: 2 }];
+  const scope = nock('http://localhost:80')
+    .get('/api/v1/todo')
+    .reply(200, fakeTodo);
+  const secondExpected = {
+    type: 'GET_TODOS',
+    isFetching: false,
+    todos: fakeTodo,
+    notification: 1
+  };
+  const dispatch = jest.fn().mockImplementationOnce(action => {
+    expect(action).toEqual(secondExpected);
+    scope.done();
+    done();
+  });
+  actions.getTodosAction()(dispatch);
+});
+
 // test('receive weight action creator', () => {
 //   const fakeWeight = '20, 30';
 
